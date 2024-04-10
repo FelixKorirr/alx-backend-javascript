1e418dcf2ef9fb89c4dc4a2f1e7a3a14d813696e
Felix Kipkorir <kipronofelix63@gmail.com>
2024-04-10 17:05:27 +0300

    ES6 promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const x = 5 + 3;
+      if (x === 8) {
+        resolve(true);
+      } else {
+        reject(new Error('value is not equal to 8'));
+      }
+    }, 2000);
+  });
+}
+
+getResponseFromAPI().then((result) => {
+  console.log(result);
+}).catch((result) => {
+  console.log(result);
+});
