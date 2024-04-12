798b737273762aead52f00050b1648a65a4afd82
Felix Kipkorir <kipronofelix63@gmail.com>
2024-04-12 17:18:41 +0300

    ES6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom class.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new classroom.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
