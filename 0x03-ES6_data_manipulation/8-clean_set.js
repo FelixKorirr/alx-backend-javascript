581cfe6b3ddd6a4fb77c2eaf5df523044448d9c2
Felix Kipkorir <kipronofelix63@gmail.com>
2024-04-18 16:44:27 +0300

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
   const p = [];
   if (
-    !set ||
-    !startString ||
-    !(set instanceof Set) ||
-    typeof startString !== 'string'
+    !set
+    || !startString
+    || !(set instanceof Set)
+    || typeof startString !== 'string'
   ) {
-    return "";
+    return '';
   }
   for (const v of set.values()) {
-    if (typeof v === "string" && v.startsWith(startString)) {
+    if (typeof v === 'string' && v.startsWith(startString)) {
       const valueSubStr = v.substring(startString.length);
 
       if (valueSubStr && valueSubStr !== v) {
@@ -17,5 +17,5 @@ export default function cleanSet(set, startString) {
       }
     }
   }
-  return p.join("-");
+  return p.join('-');
 }
