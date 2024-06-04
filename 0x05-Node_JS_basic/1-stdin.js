1fc63e6e28d88a65df8aa2153967640f2d0c2dc5
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-04 16:43:42 +0300

    nodeJS basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  name = data.toString().trim()
+  const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
