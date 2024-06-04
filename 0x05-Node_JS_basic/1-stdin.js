a60c7be4eeaa5cb738c9a236abac1a04115f3d6c
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-04 16:40:24 +0300

    nodeJS basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name\n');
-
-process.stdin.on('data', (name) => {
-  console.log('Your name is: ', name);
+process.stdin.on('data', (data) => {
+  name = data.toString().trim()
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
