e7d848b3d6d226ae0aa016fc1d33d1d2b7d283ba
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-04 15:09:26 +0300

    nodeJS basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-const readline = require("readline");
+const readline = require('readline');
 
-rlInterface = readline.createInterface(process.stdin, process.stdout);
+const rlInterface = readline.createInterface(process.stdin, process.stdout);
 
 rlInterface.question(
-  "Welcome to Holberton School, what is your name?\n",
+  'Welcome to Holberton School, what is your name?\n',
   (name) => {
-    console.log("Your name is " + name);
-    console.log("This important software is now closing");
+    console.log('Your name is ', name);
+    console.log('This important software is now closing');
     rlInterface.close();
-  }
+  },
 );
