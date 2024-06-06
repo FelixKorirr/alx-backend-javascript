9ff5c09c5501a0235cc7a1f2468f118ca8b73f0a
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-06 16:32:22 +0300

    nodeJS basics

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,6 +4,8 @@ const host = '127.0.0.1';
 const port = 1245;
 
 const myServer = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
 });
 
