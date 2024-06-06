a8a3ac54d01ba26cb92da253669842e389878fcc
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-06 15:54:08 +0300

    nodeJS basics

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,52 +1,47 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  if (!fs.existsSync(path)) {
+async function countStudents(path) {
+  let data;
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error(err));
-      }
 
-      const myData = [];
-      const csStudents = [];
-      const sweStudents = [];
+  const myData = [];
+  const csStudents = [];
+  const sweStudents = [];
 
-      data.split('\n').forEach((line, index) => {
-        if (index === 0) return;
-        const lineArr = line.split(',');
-        const myObj = {
-          firstname: lineArr[0],
-          lastname: lineArr[1],
-          age: lineArr[2],
-          field: lineArr[3],
-        };
-        myData.push(myObj);
-      });
-
-      for (const line of myData) {
-        if (line.field === 'CS') {
-          csStudents.push(line.firstname);
-        } else if (line.field === 'SWE') {
-          sweStudents.push(line.firstname);
-        }
-      }
-      console.log(`Number of students: ${myData.length}`);
-      console.log(
-        `Number of students in CS: ${
-          csStudents.length
-        }. List: ${csStudents.join(', ')}`,
-      );
-      console.log(
-        `Number of students in SWE: ${
-          sweStudents.length
-        }. List: ${sweStudents.join(', ')}`,
-      );
-      resolve();
-    });
+  data.split('\n').forEach((line, index) => {
+    if (index === 0) return;
+    const lineArr = line.split(',');
+    const myObj = {
+      firstname: lineArr[0],
+      lastname: lineArr[1],
+      age: lineArr[2],
+      field: lineArr[3],
+    };
+    myData.push(myObj);
   });
+
+  for (const line of myData) {
+    if (line.field === 'CS') {
+      csStudents.push(line.firstname);
+    } else if (line.field === 'SWE') {
+      sweStudents.push(line.firstname);
+    }
+  }
+  console.log(`Number of students: ${myData.length}`);
+  console.log(
+    `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(
+      ', ',
+    )}`,
+  );
+  console.log(
+    `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(
+      ', ',
+    )}`,
+  );
 }
 
 module.exports = countStudents;
