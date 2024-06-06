06bb1da87281f91211d1ebafc68ee27cfeec8017
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-06 16:39:10 +0300

    simple HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,9 +6,9 @@ const port = 1245;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-server.listen(port, host)
+server.listen(port, host);
 
 module.exports = server;
