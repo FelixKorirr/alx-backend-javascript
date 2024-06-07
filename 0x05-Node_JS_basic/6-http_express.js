56192718ca38a94ca437a0d03f157c1780e464b3
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-07 12:53:12 +0300

    simple http Server with express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,10 +4,13 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-	res.statusCode=200;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+module.exports = app;
