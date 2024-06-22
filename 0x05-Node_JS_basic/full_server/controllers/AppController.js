7ab4667689e2dd452180c148e9bf42b932949192
Felix Kipkorir <kipronofelix63@gmail.com>
2024-06-22 10:41:43 +0300

    full_server

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,11 @@
+/**
+ * Contains the miscellaneous route handlers.
+ */
 class AppController {
-  static getHomepage(_request, response) {
-    response.send(200, 'Hello Holberton School!');
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
 export default AppController;
+module.exports = AppController;
